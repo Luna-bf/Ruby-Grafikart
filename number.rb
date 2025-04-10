b448b8c8d2491a9388007e987d9bb6d90edab2a1
Luna-bf <lburattifayolle@gmail.com>
2025-04-10 15:45:22 +0200

    Changed then deleted some things

--- number.rb
+++ number.rb
@@ -1,3 +1,5 @@
+# Les conditions
+
 puts("Veuillez entrer un nombre :")
 
 userNumber = gets.chomp.to_i # .to_i sert à convertir une valeur en Integer, car par défaut, chomp retourne une chaîne de caractères
