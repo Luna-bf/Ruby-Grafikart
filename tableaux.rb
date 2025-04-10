7e5eec98de290adadffe7b497ca9027ada181953
Luna-bf <lburattifayolle@gmail.com>
2025-04-10 16:45:25 +0200

    Added an explanation on how to get an array's element

--- tableaux.rb
+++ tableaux.rb
@@ -1,3 +1,8 @@
 # Les tableaux, les hashs et les symboles
 
 exemple = [ 1234, "a string", true ] # Un tableau peut contenir : des entiers, des strings et des booléens
+
+students = ['John', 'Jane', 'David']
+
+# Pour récupérer un élément du tableau, je fais nom_de_mon_tableau[1]
+puts students[1] # Attention, l'index commence à zéro, récupérer l'élément ayant l'index 1 me donnera 'Jane'
