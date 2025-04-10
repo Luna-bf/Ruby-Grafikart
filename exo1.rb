2eee7cde913e2c613cdcced0b3f44ba131f9a635
Luna-bf <lburattifayolle@gmail.com>
2025-04-10 10:31:42 +0200

    First Ruby script

--- exo1.rb
+++ exo1.rb
@@ -1,2 +1,5 @@
-puts ("Hello world !")
-puts ("Salut les gens !")
+puts("Veuillez entrer votre nom :") # puts va m'afficher la valeur contenue entre parenthèses dans le terminal
+
+#La méthode chomp permet de supprimer les sauts de ligne
+prenom = gets.chomp # Je déclare une variable qui va devoir récupérer une valeur
+puts "Bonjour #{prenom}, comment allez vous ?" # puts n'affichera pas la String tout de suite car il attendra que je lui donne une valeur qui sera ensuite rangée dans la variable prenom
