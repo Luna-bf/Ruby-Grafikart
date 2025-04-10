7f879cd6900087ddb6942c10dced08068f4e2cc9
Luna-bf <lburattifayolle@gmail.com>
2025-04-10 13:25:31 +0200

    (Upgraded) Ruby script checking if a number is odd or even

--- number.rb
+++ number.rb
@@ -1,4 +1,9 @@
 puts("Veuillez entrer un nombre :")
 
-userNumber = gets.chomp.to_i # .to_i sert à convertir une String en Integer, car par défaut, chomp retourne une chaîne de caractères
-puts userNumber.even?
+userNumber = gets.chomp.to_i # .to_i sert à convertir une valeur en Integer, car par défaut, chomp retourne une chaîne de caractères
+
+if userNumber.even? === true
+    puts "Votre nombre est pair"
+else
+    puts "Votre nombre est impair"
+end
